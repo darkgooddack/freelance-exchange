b0d08ac99eefff9eb0bc168e4a2e67f65ec6d68d
darkgooddack <ulyanaworkpage@gmail.com>
2025-01-27 12:51:54 +0300

    feat: changing models, configuring urls

--- backend/django_freelance/freelance/models.py
+++ backend/django_freelance/freelance/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Create your models here.
 class Executor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=11)
@@ -20,50 +20,53 @@ class Customer(models.Model):
 
 class Service(models.Model):
     SERVICE_TYPES = [
-        ('1', 'Веб-разработка'),
-        ('2', 'Маркетинг'),
-        ('3', 'Копирайтинг'),
-        ('4', 'СЕО-продвижение'),
-        ('5', 'Переводы'),
-        ('6', 'Видеомонтаж'),
-        ('7', 'Фотография')
-    ]
+                ('1', 'Веб разработка'),
+                ('2', 'Маркетинг'),
+                ('3', 'Копирайтинг'),
+                ('4', 'Рерайтиинг'),
+                ('5', 'Переводы'),
+                ('6', 'Видеомонтаж'),
+                ('7', 'Фотография')
+            ]
 
     executor = models.ForeignKey(Executor, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
-    description = models.CharField(max_length=1000)
+    desc = models.CharField(max_length=1000)
     price = models.IntegerField()
     service_type = models.CharField(choices=SERVICE_TYPES, default='1', max_length=1)
 
     def __str__(self):
-        return "{}, {}, price: {}".format(self.name, self.get_service_type_display(), self.price)
+        return '{}, {}, price: {}'.format(self.name, self.get_service_type_display(), self.price)
 
 
 class Order(models.Model):
-    SERVICE_TYPES = [
-        ('1', 'Веб-разработка'),
-        ('2', 'Маркетинг'),
-        ('3', 'Копирайтинг'),
-        ('4', 'СЕО-продвижение'),
-        ('5', 'Переводы'),
-        ('6', 'Видеомонтаж'),
-        ('7', 'Фотография')
-    ]
+    ORDER_TYPES = [
+                ('1', 'Веб разработка'),
+                ('2', 'Маркетинг'),
+                ('3', 'Копирайтинг'),
+                ('4', 'Рерайтиинг'),
+                ('5', 'Переводы'),
+                ('6', 'Видеомонтаж'),
+                ('7', 'Фотография')
+            ]
 
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
-    description = models.CharField(max_length=1000)
+    desc = models.CharField(max_length=1000)
     price = models.IntegerField()
-    order_type = models.CharField(choices=SERVICE_TYPES, default='1', max_length=1)
+    order_type = models.CharField(choices=ORDER_TYPES, default='1', max_length=1)
 
     def __str__(self):
-        return "{}, {}, price: {}".format(self.name, self.get_order_type_display(), self.price)
+        return '{}, {}, price: {}'.format(self.name, self.get_order_type_display(), self.price)
 
 
 class Tag(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 class Ordering(models.Model):
@@ -73,6 +76,9 @@ class Ordering(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
     order_date = models.DateTimeField()
     deadline = models.DateTimeField()
+
+    def __str__(self):
+        return '{} - {}, Customer: {}, Executor: {}'.format(self.order_date, self.deadline, self.customer, self.executor)
 
 
 class Message(models.Model):
@@ -85,14 +91,15 @@ class Message(models.Model):
 
 class Ticket(models.Model):
     SEVERITIES = [
-        ('1', 'Низкая'),
-        ('2', 'Средняя'),
-        ('3', 'Высокая')
-    ]
+                ('1', 'Низкая'),
+                ('2', 'Средняя'),
+                ('3', 'Высокая')
+            ]
 
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True, null=True)
     executor = models.ForeignKey(Executor, on_delete=models.CASCADE, blank=True, null=True)
     severity = models.CharField(choices=SEVERITIES, default='1', max_length=1)
+    desc = models.CharField(max_length=1000, default='default_value')
     ticket_date = models.DateTimeField()
     is_resolved = models.BooleanField(default=False)
 
@@ -101,16 +108,16 @@ class Ticket(models.Model):
 
 
 class Review(models.Model):
-    RATING_FILED = [
-        ('1', 1),
-        ('2', 2),
-        ('3', 3),
-        ('4', 4),
-        ('5', 5)
-    ]
+    RATING_FILLED = [
+                ('1', 1),
+                ('2', 2),
+                ('3', 3),
+                ('4', 4),
+                ('5', 5)
+            ]
 
-    rating = models.CharField(choices=RATING_FILED, default='1', max_length=1)
-    decs = models.CharField(max_length=1000)
+    rating = models.CharField(choices=RATING_FILLED, default='1', max_length=1)
+    desc = models.CharField(max_length=1000)
 
 
 class Authoring(models.Model):
